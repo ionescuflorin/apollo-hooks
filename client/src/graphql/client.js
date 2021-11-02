ed10c3d38308607f0c9cddba06a26eb1840a273b
Ionescu Florin-Eugen <ionescuflorin-eugen@MacBook-Pro-Ionescu.local>
2021-11-02 19:27:53 +0200

    Initial chat app

---
+++ client/src/graphql/client.js
@@ -0,0 +1,45 @@
+import {
+  ApolloClient, ApolloLink, HttpLink, InMemoryCache, split
+} from 'apollo-boost';
+import {WebSocketLink} from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+import { getAccessToken } from '../auth';
+
+const httpUrl = 'http://localhost:9000/graphql';
+const wsUrl = 'ws://localhost:9000/graphql';
+
+const httpLink = ApolloLink.from([
+  new ApolloLink((operation, forward) => {
+    const token = getAccessToken();
+    if (token) {
+      operation.setContext({headers: {'authorization': `Bearer ${token}`}});
+    }
+    return forward(operation);
+  }),
+  new HttpLink({uri: httpUrl})
+]);
+
+const wsLink = new WebSocketLink({uri: wsUrl, options: {
+  // This way the WebSocketLink instance will call this function just before starting a connection
+  // and get a connectionParams object with the latest accessToken value.
+  connectionParams: () => ({
+    accessToken: getAccessToken()
+  }),
+  // make a ws connection only when needed
+  lazy: true,
+  reconnect: true
+}})
+
+function isSubscription(operation) {
+  const definition = getMainDefinition(operation.query);
+  return definition.kind === 'OperationDefinition'
+  && definition.operation === 'subscription'
+}
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: split(isSubscription, wsLink, httpLink),
+  defaultOptions: {query: {fetchPolicy: 'no-cache'}}
+});
+
+export default client;
